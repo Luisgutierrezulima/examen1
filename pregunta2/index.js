15e7581e0939a7127b20d7c0b871d7853c1d030b
LUIS <20224815@aloe.ulima.edu.pe>
2025-04-28 21:24:15 -0500

    pregunta2

---
+++ pregunta2/index.js
@@ -0,0 +1,9 @@
+let boton=document.querySelector("boton")
+let texto=document.querySelector("exampleFormControlInput1")
+if (boton.addEventListener(onclick)){
+    let i=texto.textContent
+    document.append(
+    <figure class="figure">
+    <img src=i class="figure-img img-fluid rounded" alt=""></img>
+    </figure>)
+}
